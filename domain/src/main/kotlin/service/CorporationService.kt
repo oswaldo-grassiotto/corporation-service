3e5c87becc0ca9b4a3ea965fc8c122a3de03081f
Alexandre Lara <thealexandrelara@gmail.com>
2019-11-20 18:03:10 -0300

    feat: implement Corporation service

--- domain/src/main/kotlin/service/CorporationService.kt
+++ domain/src/main/kotlin/service/CorporationService.kt
@@ -3,7 +3,31 @@ package service
 import entity.Corporation
 import repository.CorporationRepository
 
-class CorporationService(val repository: CorporationRepository) {
-    fun createCorporation(value: Corporation){}
-    fun searchCorporationById(value: String){}
+class CorporationService(private val repository: CorporationRepository) {
+
+    fun createCorporation(value: Corporation) : String {
+        return repository.save(value)
+    }
+
+    fun searchCorporationById(value: String) : Corporation? {
+        return repository.fetchById(value)
+    }
+
+    fun searchCorporationByDocument(value: String) : Corporation? {
+        return repository.fetchByDocument(value)
+    }
+
+    fun updateCorporation(value: Corporation) : Boolean {
+        val corporation = repository.fetchById(value.id)
+        if(corporation === null){
+            return false
+        }
+
+        return repository.update(value)
+    }
+
+    fun removeCorporation(corporationId: String) : Boolean {
+        return repository.remove(corporationId)
+    }
+
 }
